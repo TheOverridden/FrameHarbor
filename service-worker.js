e50a3ac5b33a5bcfd8c955a38679b8d1e21aefa7
TheOverridden <lincmoore404@proton.me>
2026-09-22 07:34:51 -0500

    Force browsers onto the new fallback client

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "frameharbor-shell-v3";
+const CACHE = "frameharbor-shell-v4";
 const SHELL = ["./", "./index.html", "./styles.css", "./app.js", "./lib.js", "./assets/icon.svg", "./manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
